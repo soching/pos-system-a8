0d8bfdfdc0dc7748b859e823b15873674d1dbc2f
CHANNY.CHHUN <CHANNY.CHHUN@PNC.LAN>
2023-12-19 10:17:01 +0700

    #30 write code in js to subtract stock

--- SCRIPT/main.js
+++ SCRIPT/main.js
@@ -92,69 +92,100 @@ let searchName = document.querySelector("#search-input");
 
 function createCardPay(event) {
     let isNotExit = true;
-    let cardContent = document.querySelector('.product-incart');
-    let productName = event.target.parentElement.children[1].textContent;
-    let productStock = parseInt(event.target.parentElement.children[3].textContent);
-    let productPrice = parseInt(event.target.parentElement.children[4].textContent);
+    // ===================================================================
+    let tr = event.target.closest("tr").children;
+    console.log(tr);
+    let cardContent = document.querySelector(".product-incart");
+    let productName = tr[1].textContent;
+    let productStock = tr[3];
+    let productPrice = parseInt(tr[4].textContent);
 
     for (let Quantitys of cardContent.children) {
         let a = Quantitys.children[0].textContent;
         if (a === productName) {
             isNotExit = false;
-            if (productStock > (Quantitys.children[1].children[0].textContent)) {
-                Quantitys.children[1].children[0].textContent = parseInt(Quantitys.children[1].children[0].textContent) + 1;
-                Quantitys.children[2].children[0].textContent = parseInt(productPrice) * parseInt(Quantitys.children[1].children[0].textContent) + "$";
-            }
         }
     }
-    if (isNotExit) {
-        let cardPay = document.createElement('div');
-        cardPay.className = 'cardpay';
 
-        let cardNamePay = document.createElement('p');
+
+    if (isNotExit && productStock.textContent > 0) {
+        let cardPay = document.createElement("div");
+        cardPay.className = "cardpay";
+        let cardNamePay = document.createElement("p");
         cardNamePay.textContent = productName;
 
-        let cardQuality = document.createElement('p');
+        let cardQuality = document.createElement("p");
         cardQuality.textContent = "Quantity: ";
 
-        let quantitySpan = document.createElement('span');
-        quantitySpan.textContent = 1;
+        let quantitySpan = document.createElement("span");
+        quantitySpan.textContent = 0;
         cardQuality.appendChild(quantitySpan);
 
-        let cardPricePay = document.createElement('p');
+        let cardPricePay = document.createElement("p");
         cardPricePay.textContent = "Price: ";
 
-        let priceSpan = document.createElement('span');
+        let priceSpan = document.createElement("span");
         priceSpan.textContent = productPrice + "$";
         cardPricePay.appendChild(priceSpan);
-
-        let bntDeleteCard = document.createElement('button');
-        bntDeleteCard.className = 'bntdelet';
-        bntDeleteCard.textContent = 'delete';
+        let bntDeleteCard = document.createElement("button");
+        bntDeleteCard.className = "bntdelet";
+        bntDeleteCard.textContent = "delete";
         cardPay.appendChild(cardNamePay);
         cardPay.appendChild(cardQuality);
         cardPay.appendChild(cardPricePay);
         cardPay.appendChild(bntDeleteCard);
         cardContent.appendChild(cardPay);
 
-        bntDeleteCard.addEventListener('click', delet);
+        bntDeleteCard.addEventListener("click", delet);
     }
-    //calculate total
+    // =====================code stock price ========================
     let total = 0;
-    document.querySelector('.final-amount').textContent = '';
-    for (const card of document.querySelectorAll(".cardpay")) {
-        let quantity = card.firstElementChild.nextElementSibling.firstElementChild.textContent;
-        let price = card.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.textContent.replace("$", "");
-        if (quantity.length > 1){
-            for (let i = 0; i < quantity.length; i++){
-                total += price;
+    const cp = document.querySelector(".product-incart").children;
+
+    for (let Quantitys of cardContent.children) {
+        let a = Quantitys.children[0].textContent;
+        if (a === productName) {
+            if (
+                productStock.textContent > 0 ||
+                productStock.textContent > Quantitys.children[1].children[0].textContent
+            ) {
+                tr[3].textContent = productStock.textContent - 1;
+                Quantitys.children[1].children[0].textContent =
+                    parseInt(Quantitys.children[1].children[0].textContent) + 1;
+                let b = (Quantitys.children[2].children[0].textContent =
+                    parseInt(productPrice) *
+                    parseInt(Quantitys.children[1].children[0].textContent) +
+                    "$");
+                // // Extract number from string using regex \d
+                let c = document.querySelector(".final-amount");
+                // // Add to total value based on the available incart
+                for (let j = 0; j < cp.length; j++) {
+                    total += parseInt(cp[j].children[2].textContent.match(/\d+/)[0]);
+                }
+                c.textContent = "Price: " + total + "$";
             }
-        }else {
-            total += parseInt(price);
         }
     }
-    document.querySelector(".final-amount").textContent = "total " + total + "$";
+
 }
+// =====================================================================
+
+//calculate total
+let total = 0;
+document.querySelector('.final-amount').textContent = '';
+for (const card of document.querySelectorAll(".cardpay")) {
+    let quantity = card.firstElementChild.nextElementSibling.firstElementChild.textContent;
+    let price = card.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.textContent.replace("$", "");
+    if (quantity.length > 1) {
+        for (let i = 0; i < quantity.length; i++) {
+            total += price;
+        }
+    } else {
+        total += parseInt(price);
+    }
+}
+document.querySelector(".final-amount").textContent = "total " + total + "$";
+
 
 function delet(event) {
     if (confirm('Are you sure you want to remove the order?')) {
